862a24c497fbc6662d03833e9d5c8a8b9bd147e6
josecaos <jazzvoon@gmail.com>
2019-08-14 17:17:53 -0500

    Listo modulo: lista de misiones

--- client/src/components/launches/LaunchItem.js
+++ client/src/components/launches/LaunchItem.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
-export default function LaunchItem(props) {
+export default function LaunchItem({launch: {flight_number, mission_name, launch_date_local, launch_success} }) {
 
-  console.log(props.launch)
 
   return (
-      <div>
-        Test
+      <div className="card card-body">
+        <div className="row">
+          <div className="col-md-9">
+            <h4 className="col-12   ">Mission:</h4>
+            <p className="col-12"> {mission_name}</p>
+            <p className="col-12"> {launch_date_local}</p>
+          </div>
+          <div className="col-md-3">
+            <button className="btn btn-primary my-auto">Launch Details</button>
+          </div>
+        </div>
       </div>
     )
 }
